768507d3970e0dc0b3aade2fb11c00aff0bfb33a
hazem_said <124845122+hazzemSaid@users.noreply.github.com>
2025-01-07 17:45:22 +0200

    Time: 1 ms (59.41%), Space: 13.7 MB (9.78%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
         vector<string>ans;
+        //sort the words from big lenght ot small 
          sort(words.rbegin(),words.rend(),[](auto a,auto b){
             return a.size()<b.size();
         });
-        unordered_set<string>f;
         int sz=words.size();
-        for(int i=0;i<sz;i++){
-            cout<<words[i]<<endl;
-            if(f.find(words[i]) != f.end()){
-                ans.push_back(words[i]);
-            }
-            for(int j=0;j<words[i].size();j++){
-                for(int k=1;k<words[i].size();k++){
-                      string substring= words[i].substr(j,k);
-                cout<<substring<<endl;
-                f.insert(substring);
+        //make the boolen for dubilcate substring
+        bool vis[3002];
+        for(int i=0;i<words.size();i++){
+            for(int j=i+1;j<words.size();j++){
+                if(vis[j] == true)continue;
+                //if i find the words[j] into words[i]
+                auto find=words[i].find(words[j]);
+                if(find != string::npos){
+                    vis[j]=true;
+                    ans.push_back(words[j]);
                 }
             }
-        }    
+        }
         return ans;
     }
 };
