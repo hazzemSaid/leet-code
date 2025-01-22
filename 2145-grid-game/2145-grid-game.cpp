6007616b3ed8bc215f5d7e94b1e9e641868963bc
hazem_said <124845122+hazzemSaid@users.noreply.github.com>
2025-01-22 19:05:15 +0200

    Time: 2 ms (64.73%), Space: 75.5 MB (41.07%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
-        long long firstRowSum = accumulate(begin(grid[0]), end(grid[0]), 0LL),
-                  secondRowSum = 0;
-        long long minimumSum = LONG_LONG_MAX;
-        for (int turnIndex = 0; turnIndex < grid[0].size(); ++turnIndex) {
-            firstRowSum -= grid[0][turnIndex];
-            // Find the minimum maximum value out of firstRowSum and
-            // secondRowSum.
-            minimumSum = min(minimumSum, max(firstRowSum, secondRowSum));
-            secondRowSum += grid[1][turnIndex];
+        int n=grid[0].size();
+        vector<long long>prfix(n),sufix(n);
+        prfix[0]=grid[0][0];
+        for(int i=1;i<n;i++)prfix[i]+=grid[0][i]+prfix[i-1];
+        sufix[n-1]=grid[1][n-1];
+        for(int i=n-2;i>=0;i--)sufix[i]+=grid[1][i]+sufix[i+1];
+        long long st1=prfix[n-1];
+        long long st2=sufix[0];
+        // for(int i=0;i<n;i++)cout<<prfix[i]<<" ";
+        // cout<<endl;
+        // for(int i=0;i<n;i++)cout<<sufix[i]<<" ";
+        // cout<<endl;
+        long long maxx=LONG_LONG_MAX;
+        for(int i=0;i<n;i++){
+          maxx=min(maxx*1ll,1ll*max(st2-sufix[i],st1-prfix[i]));
         }
-        return minimumSum;
+     
+        return maxx;
     }
 };
