cedaaf21d345d010d4a4d87b30f8d204a7657ae4
hazem_said <124845122+hazzemSaid@users.noreply.github.com>
2025-01-24 14:14:04 +0200

    Time: 121 ms (5.32%), Space: 73.9 MB (5.02%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-       
         map<int,vector<int>>mp;
         priority_queue<int>t;
         int n=graph.size();
-        vector<int>ans;
         vector<int>out(n);
         for(int i=0;i<n;i++){
             for(auto &it:graph[i]){
@@ -14,15 +12,19 @@ public:
             }
             if(out[i] == 0)t.push(i);
         }
+        vector<bool>vis(n,false);
         while(!t.empty()){
             auto it=t.top();
             t.pop();
-            ans.push_back(it);
+            vis[it]=1;
             for(auto &i:mp[it]){
                 if(--out[i] == 0)t.push(i);
             }
         }
-        sort(ans.begin(),ans.end());
+        vector<int>ans;
+        for(int i=0;i<n;i++){
+            if(vis[i])ans.push_back(i);
+        }
         return ans;
     }
 };
