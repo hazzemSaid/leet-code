671d1085c4968bd1a3271f5a3c613bd53065a645
hazem_said <124845122+hazzemSaid@users.noreply.github.com>
2025-09-18 09:57:45 +0300

    Time: 242 ms (80%), Space: 366.1 MB (67.5%) - LeetHub

--- 3678-design-task-manager/3678-design-task-manager.cpp
+++ 3678-design-task-manager/3678-design-task-manager.cpp
@@ -1,78 +1,63 @@
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
 class TaskManager {
+    struct Task {
+        int pr;
+        int taskId;
+        bool operator<(const Task& other) const {
+            if (pr == other.pr) return taskId < other.taskId; 
+            return pr < other.pr; // higher priority first
+        }
+    };
+
+    priority_queue<Task> pq;
+    unordered_map<int,int> tofinduser; // taskId -> userId
+    unordered_map<int,int> taskpr;     // taskId -> pr
+
 public:
-//taskid - > userid
-map<int,int>tofinduser;
-map<int,set<int>>higherproirtytaskfind;
-// -pr -> front * -1 -> higher task
-map<int,int>taskpr;
-//taskid -> pr
-// void printx(){
-//     cout<<"----------------------"<<endl;
-//      for(auto it:higherproirtytaskfind){
-//             cout<<it.first<<"->";
-//             for(auto item:it.second){
-//                 cout<<item<<" ";
-//             }
-//             cout<<endl;
-//         }
-// }
     TaskManager(vector<vector<int>>& tasks) {
-        for(auto it:tasks){
-            int userid=it[0];
-            int taskid=it[1];
-            int pr=it[2];
-            tofinduser[taskid]=userid;
-            taskpr[taskid]=pr;
-            higherproirtytaskfind[-pr].insert(-taskid);
+        for (auto &t : tasks) {
+            int userId = t[0], taskId = t[1], pr = t[2];
+            tofinduser[taskId] = userId;
+            taskpr[taskId] = pr;
+            pq.push({pr, taskId});
         }
-    //    printx();
     }
-    
+
     void add(int userId, int taskId, int priority) {
-          tofinduser[taskId]=userId;
-            taskpr[taskId]=priority;
-            higherproirtytaskfind[-priority].insert(-taskId);
-            // printx();
+        tofinduser[taskId] = userId;
+        taskpr[taskId] = priority;
+        pq.push({priority, taskId});
     }
-    
+
     void edit(int taskId, int newPriority) {
-        int oldpr=taskpr[taskId];
-        higherproirtytaskfind[-oldpr].erase(-taskId);
-        if( higherproirtytaskfind[-oldpr].empty()){
-             higherproirtytaskfind.erase(-oldpr);
-        }
-        higherproirtytaskfind[-newPriority].insert(-taskId);
-        taskpr[taskId]=newPriority;
-        // printx();
+        if (taskpr.find(taskId) == taskpr.end()) return;
+        taskpr[taskId] = newPriority;
+        pq.push({newPriority, taskId}); // old version left in heap (lazy deletion)
     }
-    
+
     void rmv(int taskId) {
-        cout<<taskId<<endl;
-        int oldpr=taskpr[taskId];
         tofinduser.erase(taskId);
         taskpr.erase(taskId);
-        higherproirtytaskfind[-oldpr].erase(-taskId);
-          if( higherproirtytaskfind[-oldpr].empty()){
-             higherproirtytaskfind.erase(-oldpr);
-        }
-        // cout<<"rmv----------"<<endl;
-        // printx();
+        // lazy deletion: don't touch pq
     }
-    
+
     int execTop() {
-        // printx();
-        if(higherproirtytaskfind.empty())return -1;
-        int res=tofinduser[(*higherproirtytaskfind.begin()->second.begin()) * -1];
-        rmv((*higherproirtytaskfind.begin()->second.begin())*-1);
-        return res;
+        while (!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+
+            // check validity
+            if (taskpr.find(top.taskId) != taskpr.end() && taskpr[top.taskId] == top.pr) {
+                int userId = tofinduser[top.taskId];
+                rmv(top.taskId);
+                return userId;
+            }
+        }
+        return -1;
     }
 };
-
-/**
- * Your TaskManager object will be instantiated and called as such:
- * TaskManager* obj = new TaskManager(tasks);
- * obj->add(userId,taskId,priority);
- * obj->edit(taskId,newPriority);
- * obj->rmv(taskId);
- * int param_4 = obj->execTop();
- */
