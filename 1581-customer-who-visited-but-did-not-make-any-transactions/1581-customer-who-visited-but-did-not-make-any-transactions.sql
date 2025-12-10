e4a87a94d5c0532d021cb98f5d4bf8fd58d41937
hazem_said <124845122+hazzemSaid@users.noreply.github.com>
2025-12-10 15:22:13 +0200

    Time: 1168 ms (82.97%), Space: 0B (100%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,16 @@
+select 
+    customer_id,
+    count(customer_id) as count_no_trans
+from 
+    Visits
+where
+     visit_id 
+not in
+(
+select 
+    Transactions.visit_id
+from 
+    Transactions
+)
+group by 
+    customer_id;
